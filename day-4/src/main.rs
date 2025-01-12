56141e1f1fbd09e079591b378a7e2e92d4ac303d
Hum Ler <hum.ler@outlook.com>
2025-01-12 22:02:07 +0800

    Add day 4

---
+++ day-4/src/main.rs
@@ -0,0 +1,83 @@
+use std::ops::RangeInclusive;
+
+use anyhow::{anyhow, Result};
+
+use aoc_cli::{get_part, Part};
+
+fn main() {
+    match get_part("inputs/day-4.txt") {
+        Ok(Part::Part1(input)) => println!("{:?}", part_1(input)),
+        Ok(Part::Part2(input)) => println!("{:?}", part_2(input)),
+        Err(error) => println!("{:?}", error),
+    }
+}
+
+fn part_1(input: String) -> Result<usize> {
+    Ok(input
+        .split_terminator("\n")
+        .map(parse_line_into_ranges)
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .filter(|(first, second)| {
+            (first.contains(second.start()) && first.contains(second.end()))
+                || (second.contains(first.start()) && second.contains(first.end()))
+        })
+        .count())
+}
+
+fn part_2(input: String) -> Result<usize> {
+    Ok(input
+        .split_terminator("\n")
+        .map(parse_line_into_ranges)
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .filter(|(first, second)| first.contains(second.start()) || second.contains(first.start()))
+        .count())
+}
+
+fn parse_line_into_ranges(line: &str) -> Result<(RangeInclusive<u8>, RangeInclusive<u8>)> {
+    let Some((first, second)) = line.split_once(",") else {
+        return Err(anyhow!("Cannot split line into ranges: {}", line));
+    };
+
+    let Some((first_left, first_right)) = first.split_once("-") else {
+        return Err(anyhow!("Cannot split range into boundaries: {}", first));
+    };
+
+    let Some((second_left, second_right)) = second.split_once("-") else {
+        return Err(anyhow!("Cannot split range into boundaries: {}", second));
+    };
+
+    Ok((
+        (first_left.parse()?..=first_right.parse()?),
+        (second_left.parse()?..=second_right.parse()?),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = r"
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
+
+    #[test]
+    fn example_1() -> Result<()> {
+        assert_eq!(part_1(EXAMPLE_INPUT.trim().to_string())?, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn example_2() -> Result<()> {
+        assert_eq!(part_2(EXAMPLE_INPUT.trim().to_string())?, 4);
+
+        Ok(())
+    }
+}
